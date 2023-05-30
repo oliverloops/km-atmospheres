9e8fe1396b85e6a3b48e2df8e447d946c758b126
oliverloops <oliveralr@protonmail.com>
2023-05-30 00:16:20 -0600

    image optimizations added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
